bab7823ddab4734fcdcfb950a5c5604da8c2bfb8
19ben <19ben@192.168.10.113>
2021-09-26 19:21:53 -0400

    no comments

--- ScrabbleBLL/src/main/java/pkgCore/Dictionary.java
+++ ScrabbleBLL/src/main/java/pkgCore/Dictionary.java
@@ -56,9 +56,6 @@ public class Dictionary {
 	 */
 	public ArrayList<Word> GenerateWords(String strLetters) {
 		ArrayList<String> combinWords = new ArrayList<String>();
-		//TODO: Generate the combinations based on incoming strLetters
-		//testing 
-		//TODO: Take the combinations computed and call GeneratePossibleWords(ArrayList<String>)
 		for (int b = 1; b < strLetters.length() + 1; b++) {
 			Iterator<int[]> iterWord = CombinatoricsUtils.combinationsIterator(strLetters.length(), b);
 			while (iterWord.hasNext()) {
@@ -74,14 +71,12 @@ public class Dictionary {
 			System.out.println(s);
 		}
 		ArrayList<Word> WordsPermut = GeneratePossibleWords(combinWords);
-		//	Here's how you sort
 		Collections.sort(WordsPermut, Word.CompWord);
 		return WordsPermut;
 	}
 	
 	private ArrayList<Word> GeneratePossibleWords(ArrayList<String> arrLetters) {
 		HashSet<Word> words = new HashSet<Word>();
-		//TODO: Call GeneratePossibleWords(String) for each String in arrLetters
 		for (String strPossibleWord : arrLetters) {
 			words.addAll(GeneratePossibleWords(strPossibleWord));
 		}
@@ -92,7 +87,6 @@ public class Dictionary {
 
 	private HashSet<Word> GeneratePossibleWords(String strLetters) {
 		HashSet<Word> hsPossibleWords = new HashSet<Word>();
-		//TODO: Insert the code that will generate the permutations
 		ArrayList<Character> arrLetters = new ArrayList<Character>();
 
 		for (int i = 0; i < strLetters.length(); i++) {
@@ -141,27 +135,16 @@ public class Dictionary {
 			if (second.isEmpty() || second.isBlank())
 				return true;
 
-			// If we reach at the end of both strings,
-			// we are done
 			if (first.length() == 0 && second.length() == 0)
 				return true;
 
-			// Make sure that the characters after '*'
-			// are present in second string.
-			// This function assumes that the first
-			// string will not contain two consecutive '*'
 			if (first.length() > 1 && first.charAt(0) == '*' && second.length() == 0)
 				return false;
 
-			// If the first string contains '?',
-			// or current characters of both strings match
 			if ((first.length() > 1 && first.charAt(0) == '?')
 					|| (first.length() != 0 && second.length() != 0 && first.charAt(0) == second.charAt(0)))
 				return match(first.substring(1), second.substring(1));
 
-			// If there is *, then there are two possibilities
-			// a) We consider current character of second string
-			// b) We ignore current character of second string.
 			if (first.length() > 0 && first.charAt(0) == '*')
 				return match(first.substring(1), second) || match(first, second.substring(1));
 
